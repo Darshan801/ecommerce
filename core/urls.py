9a56a6e4f2a274c0770f0a23acada0b845fac2ad
Darshan801 <darshankunwar79@gmail.com>
2025-12-21 22:56:06 +0545

    frontend_template_added

--- core/urls.py
+++ core/urls.py
@@ -6,5 +6,8 @@ app_name = "core"
 
 urlpatterns = [
     path('',views.index,name='index'),
+    path('contact/',views.contact,name='contact'),
+    path('about/',views.about,name='about'),
+    path('signin/',views.sign_in,name='sign_in'),
 ]
 
--- core/views.py
+++ core/views.py
@@ -4,3 +4,12 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return render(request,'core/index.html')
+
+def contact(request):
+    return render(request,'core/contact.html')
+
+def about(request):
+    return render(request,'core/about.html')
+
+def sign_in(request):
+    return render(request,'core/signin.html')
